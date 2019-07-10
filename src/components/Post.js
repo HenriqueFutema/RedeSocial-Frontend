1a52298da55bd4d11edf3e1c26adbdb5c19a306b
HenriqueFutema <minifut0407@gmail.com>
2019-07-09 23:44:23 -0300

    Adicionando o momentjs

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 
-// import { Container } from './styles';
+import moment from "moment";
 
 export default class Post extends Component {
   render() {
     return (
       <div className="col col-lg-12 border rounded my-2 p-4">
-        <p className="float-right">{this.props.post.createdAt}</p>
+        <p className="float-right">
+          {moment(this.props.post.createdAt).format("MMMM Do YYYY, h:mm:ss a")}
+        </p>
         <h4>{this.props.post.author.name}</h4>
         <h6>{this.props.post.content}</h6>
         <p>
